586b7b1c7a0eb51ce4f1ff63769a3cf23233cc7d
matthew-dappit <125866971+MattStrauss02@users.noreply.github.com>
2025-09-20 09:48:04 +0200

    Initial Commit

---
+++ WorkoutBuddyTests/WorkoutBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutBuddyTests.swift
+//  WorkoutBuddyTests
+//
+//  Created by Matthew Strauss on 2025/09/20.
+//
+
+import Testing
+@testable import WorkoutBuddy
+
+struct WorkoutBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
